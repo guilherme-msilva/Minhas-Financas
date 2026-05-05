492ba96c05140094f6e54c591376ce52775701e8
Guilherme M. Silva <squash.cs@gmail.com>
2026-05-05 17:40:27 -0300

    Ajuste menu index

--- menu.php
+++ menu.php
@@ -2,7 +2,7 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex items-center justify-between h-16">
             <div class="flex items-center space-x-4">
-                <a href="#" class="text-white text-xl font-bold tracking-wider flex items-center space-x-2">
+                <a href="index.php" class="text-white text-xl font-bold tracking-wider flex items-center space-x-2">
                     <svg class="w-6 h-6 text-cyan-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                     <span>Finanças</span>
                 </a>
