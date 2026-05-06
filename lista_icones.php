04e663631b7be6fc95247c3b72182722b9de1e14
Guilherme M. Silva <squash.cs@gmail.com>
2026-05-06 16:24:19 -0300

    adicao icones

--- lista_icones.php
+++ lista_icones.php
@@ -1,11 +1,11 @@
 <?php
 $icones_catalogo = [
-    'ph-house', 'ph-car', 'ph-shopping-cart', 'ph-fork-knife', 'ph-hamburger','ph-heart-beat', 
-    'ph-graduation-cap', 'ph-airplane-tilt', 'ph-paw-print', 'ph-t-shirt', 
+    'ph-house', 'ph-car', 'ph-shopping-cart', 'ph-fork-knife', 'ph-hamburger','ph-heartbeat', 
+    'ph-pill', 'ph-graduation-cap', 'ph-airplane-tilt', 'ph-paw-print', 'ph-t-shirt', 
     'ph-monitor', 'ph-game-controller', 'ph-gift', 'ph-trend-up', 'ph-money', 
     'ph-piggy-bank', 'ph-lightning', 'ph-wrench', 'ph-book', 'ph-coffee', 
     'ph-film-strip', 'ph-basketball', 'ph-music-notes', 'ph-train', 'ph-bus', 
     'ph-bicycle', 'ph-gas-pump', 'ph-baby', 'ph-briefcase', 'ph-credit-card', 
-    'ph-invoice', 'ph-chart-line-up', 'ph-dots-three-circle'
+    'ph-invoice', 'ph-chart-line-up', 'ph-dots-three-circle', 'ph-empty'
 ];
 ?>
